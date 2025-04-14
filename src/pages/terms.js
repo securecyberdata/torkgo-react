f054ee634961dc51d05c57dfed9d474f7b223bd2
Shah Rukh <securecyberdata@gmail.com>
2025-04-14 14:25:04 -0700

    Vercel 1st Fix

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -51,20 +51,20 @@ const Terms = () => {
 
           <h2 id="section1">1. Why this document is important</h2>
           <p>
-            This document sets out the terms and conditions for your Planet Sayari Bank/PSB Personal and Business account (your "Account") and its related services. It also sets out other important things that you need to know.
+            This document sets out the terms and conditions for your Planet Sayari Bank/PSB Personal and Business account (your &quot;Account&quot;) and its related services. It also sets out other important things that you need to know.
           </p>
           <p>
-            These terms and conditions, along with the Fees Page and any other terms and conditions that apply to our services, form a legal agreement (the "Agreement", or the "Terms") between:
+            These terms and conditions, along with the Fees Page and any other terms and conditions that apply to our services, form a legal agreement (the &quot;Agreement&quot;, or the &quot;Terms&quot;) between:
           </p>
           <ul className="terms__list">
             <li>you, the account holder; and</li>
             <li>us, Planet Sayari Bank/PSB.</li>
           </ul>
           <p>
-            We are a Comoros Island company, with address in Hamchako, Mutsamudu, The Autonomous Island of Anjouan, Union of Comoros, with Banking License L15802/PSB released by UNION OF COMOROS OFFSHORE FINANCE AUTHORITY and for management of bank accounts for our clients. It's important for you to understand how your Account works. You can ask for a copy of these Terms through the Planet Sayari Bank/PSB app, at any time.
+            We are a Comoros Island company, with address in Hamchako, Mutsamudu, The Autonomous Island of Anjouan, Union of Comoros, with Banking License L15802/PSB released by UNION OF COMOROS OFFSHORE FINANCE AUTHORITY and for management of bank accounts for our clients. It&apos;s important for you to understand how your Account works. You can ask for a copy of these Terms through the Planet Sayari Bank/PSB app, at any time.
           </p>
           <p>
-            If you'd like more information you might find it helpful to write to our informational email support@planetsayaribank.com.
+            If you&apos;d like more information you might find it helpful to write to our informational email support@planetsayaribank.com.
           </p>
           <p>
             The main way we provide our services is through the Planet Sayari Bank/PSB mobile app. However, we provide our services in other ways too, like through web pages, meetings, video calls and other means. These terms apply whenever and however you access our services.
@@ -72,36 +72,36 @@ const Terms = () => {
 
           <h2 id="section2">2. What type of account is my Account?</h2>
           <p>
-            Your Account is a "virtual" account that holds your e-money. It may hold e-money in different currencies at the same time.
+            Your Account is a &quot;virtual&quot; account that holds your e-money. It may hold e-money in different currencies at the same time.
           </p>
           <p>
-            E-money is an electronic alternative to cash. If you or someone else gives us money in one currency, we'll issue an equivalent value of e-money in that currency. We'll store the e-money in your Account, and other people will accept it as payment. In these Terms, we use "money" to refer to e-money.
+            E-money is an electronic alternative to cash. If you or someone else gives us money in one currency, we&apos;ll issue an equivalent value of e-money in that currency. We&apos;ll store the e-money in your Account, and other people will accept it as payment. In these Terms, we use &quot;money&quot; to refer to e-money.
           </p>
 
           <h2 id="section3">3. Using money in your Account</h2>
-          <p>Once you have e-money in your Account you'll be able to use our services. For example, you can do the following:</p>
+          <p>Once you have e-money in your Account you&apos;ll be able to use our services. For example, you can do the following:</p>
           <ul className="terms__list">
             <li>send money to and receive money from other Planet Sayari Bank/PSB Accounts and non-Planet Sayari Bank/PSB Accounts;</li>
-            <li>change money from one currency to another (we call this a "currency exchange"). The currencies available might change occasionally;</li>
+            <li>change money from one currency to another (we call this a &quot;currency exchange&quot;). The currencies available might change occasionally;</li>
             <li>make payments and withdraw cash using your Planet Sayari Bank/PSB Card; and</li>
             <li>view information about and manage your Account.</li>
           </ul>
           <p>
-            We add new features and services all the time. We'll let you know about these through the Planet Sayari Bank/PSB app.
+            We add new features and services all the time. We&apos;ll let you know about these through the Planet Sayari Bank/PSB app.
           </p>
 
           <h2 id="section4">4. Can I open an Account?</h2>
           <p>
-            Normally, you must be 18 or over to open an Account. If you are under 18 and we let you have an Account or any other service, we'll let you know any special terms and conditions that apply.
+            Normally, you must be 18 or over to open an Account. If you are under 18 and we let you have an Account or any other service, we&apos;ll let you know any special terms and conditions that apply.
           </p>
           <p>
             When you ask us to open an Account, we or someone acting for us will ask for information about you and where the money you will put in your Account comes from. We do this for a number of reasons, including to check your credit score and identity, and to meet our legal and regulatory requirements.
           </p>
-          <p>You can't:</p>
+          <p>You can&apos;t:</p>
           <ul className="terms__list">
             <li>open more than one Planet Sayari Bank/PSB Personal Account;</li>
             <li>use a Planet Sayari Bank/PSB Personal Account for business purposes; or</li>
-            <li>open a new Planet Sayari Bank/PSB Personal Account if we've previously closed a Planet Sayari Bank/PSB Personal Account in your name.</li>
+            <li>open a new Planet Sayari Bank/PSB Personal Account if we&apos;ve previously closed a Planet Sayari Bank/PSB Personal Account in your name.</li>
           </ul>
 
           <h2 id="section5">5. How do I get information on payments into and out of my Account?</h2>
@@ -123,7 +123,7 @@ const Terms = () => {
             <li>contact us as soon as possible if you think someone else has accessed your Account or your security details have been compromised.</li>
           </ul>
           <p>
-            We may block your access to your Account or to our services if we're reasonably concerned about:
+            We may block your access to your Account or to our services if we&apos;re reasonably concerned about:
           </p>
           <ul className="terms__list">
             <li>the security of your Account or security details;</li>
@@ -133,10 +133,10 @@ const Terms = () => {
 
           <h2 id="section7">7. Fees and Charges</h2>
           <p>
-            Our fees are set out in our Fees Page. We'll let you know about any fees before we charge them. Some of our fees might change over time. You can always see our current fees in the Fees Page.
+            Our fees are set out in our Fees Page. We&apos;ll let you know about any fees before we charge them. Some of our fees might change over time. You can always see our current fees in the Fees Page.
           </p>
           <p>
-            We may deduct any fees or other amounts that you owe us from your Account. If you don't have enough money in your Account to pay a fee or other amount that you owe us, we may:
+            We may deduct any fees or other amounts that you owe us from your Account. If you don&apos;t have enough money in your Account to pay a fee or other amount that you owe us, we may:
           </p>
           <ul className="terms__list">
             <li>deduct the amount from any money that is paid into your Account later;</li>
@@ -159,16 +159,16 @@ const Terms = () => {
 
           <h2 id="section9">9. Closing Your Account</h2>
           <p>
-            You can close your Account at any time by contacting us. We may also close your Account by giving you at least two months' notice. In some situations, we may close or suspend your Account immediately if:
+            You can close your Account at any time by contacting us. We may also close your Account by giving you at least two months&apos; notice. In some situations, we may close or suspend your Account immediately if:
           </p>
           <ul className="terms__list">
-            <li>we suspect you're using your Account for fraudulent or illegal purposes;</li>
-            <li>you've given us false or misleading information;</li>
-            <li>you haven't provided us with information we've asked for;</li>
-            <li>you've broken these Terms in a serious or persistent way;</li>
-            <li>you're no longer eligible for an Account;</li>
+            <li>we suspect you&apos;re using your Account for fraudulent or illegal purposes;</li>
+            <li>you&apos;ve given us false or misleading information;</li>
+            <li>you haven&apos;t provided us with information we&apos;ve asked for;</li>
+            <li>you&apos;ve broken these Terms in a serious or persistent way;</li>
+            <li>you&apos;re no longer eligible for an Account;</li>
             <li>we have to do so by law; or</li>
-            <li>you've been declared bankrupt.</li>
+            <li>you&apos;ve been declared bankrupt.</li>
           </ul>
 
           <h2 id="section10">10. Data Protection and Privacy</h2>
@@ -199,10 +199,10 @@ const Terms = () => {
 
           <h2 id="section12">12. Complaints</h2>
           <p>
-            If you're unhappy with our service, please contact us first through the Planet Sayari Bank/PSB app or by email at complaints@planetsayaribank.com. We'll try to resolve your complaint as quickly as possible.
+            If you&apos;re unhappy with our service, please contact us first through the Planet Sayari Bank/PSB app or by email at complaints@planetsayaribank.com. We&apos;ll try to resolve your complaint as quickly as possible.
           </p>
           <p>
-            If you're still not happy after we've tried to resolve your complaint, you may be able to refer it to the Financial Ombudsman Service.
+            If you&apos;re still not happy after we&apos;ve tried to resolve your complaint, you may be able to refer it to the Financial Ombudsman Service.
           </p>
 
           <h2 id="section13">13. Our Legal Responsibility</h2>
@@ -218,7 +218,7 @@ const Terms = () => {
 
           <h2 id="section14">14. Transfer of Rights</h2>
           <p>
-            You cannot transfer any rights or obligations you have under these Terms to anyone else. We can transfer our rights and obligations under these Terms to another company by giving you two months' notice. If we do this, your rights under these Terms will not be affected.
+            You cannot transfer any rights or obligations you have under these Terms to anyone else. We can transfer our rights and obligations under these Terms to another company by giving you two months&apos; notice. If we do this, your rights under these Terms will not be affected.
           </p>
 
           <h2 id="section15">15. Governing Law</h2>
